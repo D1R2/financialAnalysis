aef174ed785039edb0477f4d6a8a7ea73fc33947
D1R2 <d1r2.main@gmail.com>
2017-07-07 14:34:57 -0400

    folder cleaning

--- tradeLog/tradeLogWorkspacePublic.py
+++ tradeLog/tradeLogWorkspacePublic.py
@@ -1,11 +1,42 @@
 import pandas as pd
+import sqlite3
 from tradeLogMaster import transactions
-from tradeLogMaster import trades
+from tradeLogMaster import trade
+from tradeLogMaster import processTradeQueue
+from tradeLogMaster import sqlToDataFrame
+from tradeLogMaster import replaceTable
 
+testing = True
+runTransactions = False
+runTradeQueue = False #Be sure to include "END" as the last line under 'TRADER' in the trade queue.
+export = False
+replace = False
 
-file = r"C:\Users\David\Desktop\gitHome\tradingPublic\tradeLog\sampleTradesCSV.csv"
-database = [r"C:\Users\David\Desktop\gitHome\tradingPublic\tradeLog\dbFiles\placeholder.txt"]
+if testing == True:
+    databasePaths = []
+else:
+    databasePaths = []
 
-trades(file, database)
+if runTransactions == True:
+    filePath =
+    transactions(filePath, databasePaths,)
+    print('Raw Transactions processed into clean transactions. Please move Account Statement into Account Statement folder.')
+    print('Please re-set Workspace parameters.')
 
+if runTradeQueue == True:
+    tradeQueue =
+    if testing == True:
+        processTradeQueue(tradeQueue, databasePaths)
+    else:
+        processTradeQueue(tradeQueue, databasePaths, clearQueue=True)
+    print('Please re-set Workspace parameters.')
+
+if export == True:
+    databasePath =
+    tableName =
+    df = sqlToDataFrame(databasePath,tableName)
+    df.to_csv()
+
+if replace == True:
+    replaceTable()
 
