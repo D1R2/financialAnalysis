5c6f1a3cdef4411871a3e1cffae3b8b18873297b
D1R2 <d1r2.main@gmail.com>
2017-07-13 08:13:37 -0400

    minor commits

--- analysis/analysisMaster.py
+++ analysis/analysisMaster.py
@@ -32,9 +32,44 @@ def quantQuoteData(folderPath, tableName, databasePaths):
 def pullFromDatabase(tableName, databasePath, date, times, columns):
     pass
 
-class data:
-    def __init__(self, ticker):
-        self.ticker = ticker
-        self.dataSets = []
-    def addDataSet(self, df, period, granularity, columns):
-        
+
+def openDev(df, filterSet=False):
+    # Calculate the simple probability of a given deviation from the open price.
+    # Goal is to return a df with probabilities of given move above or below open in percentage terms.
+
+    if filterSet == False:
+        filterSet = [-.05, -.045, -.04, -.035, -.03, -.025, -.02, -.0175, -.015, -.0125, -.01
+                     - .009, -.008, -.007, -.006, -.005, -.004, -.003, -.002, -.001, 0, .001,
+                     .002, .003, .004, .005, .006, .007, .008, .009, .01, .0125, .015, .0175, .02,
+                     .025, .03, .035, .04, .045, .05]
+    else:
+        pass
+
+    df['highDev'] = (df.High - df.Open) / df.Open
+    df['lowDev'] = (df.Low - df.Open) / df.Open
+    df['lowDevAbs'] = abs(df.lowDev)
+    df['closeDev'] = (df.Close - df.Open) / df.Open
+
+    dfOpenDev = pd.DataFrame(index=filterSet)
+    highDevCol = []
+    lowDevCol = []
+    bothDevCol = []
+    closeDevGreater = []
+    closeDevLess = []
+
+    for x in filterSet:
+        highDevCol.append(len(df[df.highDev > x]) / len(df))
+        lowDevCol.append(len(df[df.lowDev < x]) / len(df))
+        closeDevGreater.append(len(df[df.closeDev > x]) / len(df))
+        closeDevLess.append(len(df[df.closeDev < x]) / len(df))
+        dfFiltered = df[df.highDev > x]
+        dfFiltered = dfFiltered[dfFiltered.lowDevAbs > x]
+        bothDevCol.append(len(dfFiltered) / len(df))
+
+    dfOpenDev['highDev'] = highDevCol
+    dfOpenDev['lowDev'] = lowDevCol
+    dfOpenDev['bothDev'] = bothDevCol
+    dfOpenDev['closeDevGreater'] = closeDevGreater
+    dfOpenDev['closeDevLess'] = closeDevLess
+
+    return dfOpenDev
